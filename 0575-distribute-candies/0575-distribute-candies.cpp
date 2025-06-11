7b97caceaacf2f56576c076b1567d7bb3f9dafa1
KARTHIKRAM11 <karthikbusi2005@gmail.com>
2025-06-11 12:42:48 +0530

    Time: 113 ms (51.07%), Space: 119.9 MB (77.46%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -16,11 +16,20 @@ public:
     }
 };
 */
-
+/*
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
         return min(unordered_set<int>(candyType.begin(), candyType.end()).size(), candyType.size() / 2);
     }
 };
+*/
+
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        return min(unordered_set<int>(candyType.begin(), candyType.end()).size(), candyType.size() >> 1);
+    }
+};
+
 
