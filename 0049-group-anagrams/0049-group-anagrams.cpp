d408b824d7af1bc27eb271d0059d78a03743f4a1
KARTHIKRAM11 <karthikbusi2005@gmail.com>
2025-08-10 14:39:08 +0530

    Time: 17 ms (56.46%), Space: 24.2 MB (93.46%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> anagram;
+        for(const auto& word : strs)
+        {
+            string key = word;
+            sort(key.begin() ,key.end());
+            anagram[key].push_back(word);
+        }
+        vector<vector<string>> result;
+        result.reserve(anagram.size());
+        for(const auto& group : anagram)
+        {
+            result.push_back(group.second);
+        }
+        return result;
+    }
+};
