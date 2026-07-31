d166bca88a7a6c4af720668bba54f91ee11a6f04
KARTHIKRAM11 <karthikbusi2005@gmail.com>
2026-07-31 21:42:15 +0530

    Time: 0 ms (100%), Space: 28.2 MB (39.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count=0 , element;
-        for(int i=0 ; i<nums.size() ; i++)
-        {
-            if(count==0)
-            {
+        int count=0;
+        int element;
+
+        for(int i=0;i<nums.size();i++) {
+            if(count==0) {
+                count=1;
                 element=nums[i];
+            }
+            else if(nums[i] == element) {
                 count++;
             }
-            else if(nums[i]==element) count++;
-
             else count--;
         }
-        int counter=0;
-    for(int i=0 ; i<nums.size() ; i++)
-    {
-        if(nums[i]==element) counter++;
-    }
-    if(counter>(nums.size()/2)) return element;
-
-    return -1;
+        int count1=0;
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i]==element) count1++;
+        }
+        if(count1>(nums.size()/2)) return element;
+        return -1;
     }
 };
